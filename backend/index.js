f66b9e2374cfb6ef6a4847e82d9c26d3cce13c9f
mohdomi <omairm.s.chouhan@gmail.com>
2025-02-06 19:22:32 +0530

    added the zodvalidation in put and post routes

--- backend/index.js
+++ backend/index.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const {todoSchema , todoUpdatedSchema} = require("./types.js")
 const app = express();
 const port = 3000;
 
@@ -14,8 +15,26 @@ app.get("/todos" , (req,res)=>{
 
 
 app.post("/todo" , (req,res)=>{
-
     // a code to a single todo to the database
+
+    const loadData = req.body;
+
+    const parseLoadData = todoSchema.safeParse({
+        title : loadData.title , 
+        description  : loadData.description , 
+        completed : false
+    })
+
+    if(!parseLoadData.success){
+        res.status(411).json({
+            msg : "Invalid Inputs"
+        })
+        return;
+    }
+
+    // write some logic for like mongodb management
+
+
 
 })
 
@@ -23,5 +42,20 @@ app.post("/todo" , (req,res)=>{
 app.put("/completed" , (req,res)=>{
 
     // a code that marks wether complete or not
+
+    const loadData = req.body;
+
+    const parseLoadData = todoUpdatedSchema.safeParse({
+        id : loadData._id
+    })
+
+    if(!parseLoadData.success){
+        res.status(411).json({
+            msg : "Invalid Data input in completed route."
+        })
+        return;
+    }
+
+    // logic for marking stuff as complete in database
 
 })
